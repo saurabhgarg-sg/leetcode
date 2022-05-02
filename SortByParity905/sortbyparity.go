01da481390aee3321f7c490f6b111a4926ab8d67
Saurabh Garg <sagarg@paloaltonetworks.com>
2022-05-02 16:22:39 -0700

    add problem stmt

--- SortByParity905/sortbyparity.go
+++ SortByParity905/sortbyparity.go
@@ -1,5 +1,8 @@
 package main
 
+// Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+// Return any array that satisfies this condition.
+
 import "fmt"
 
 func main() {
